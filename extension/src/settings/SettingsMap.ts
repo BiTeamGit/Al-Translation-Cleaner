360a89885b2f3153f366277c70b4f3fa5b9e48d5
Constantin Rasch <constantin.rasch@biteam.de>
2026-04-15 09:55:14 +0200

    Added Setting

--- extension/src/settings/SettingsMap.ts
+++ extension/src/settings/SettingsMap.ts
@@ -3,4 +3,5 @@ import { Settings } from "./Settings";
 export const settingsMap = new Map<string, keyof Settings>([
     ["ATC.translationMethod", "translationMethod"],
     ["ATC.languageMapping", "languageMapping"],
+    ["ATC.WhenTranslationNotFound", "WhenTranslationNotFound"],
 ])
